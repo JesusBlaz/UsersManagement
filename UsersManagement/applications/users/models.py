527019f4541a5335ea41d421b948ee1496578204
Jesus Garrido Marquez <jesus.garrido2015@gmail.com>
2022-07-26 14:23:26 -0500

    Nuevo modelo users en Django para la gestión total de los usuarios.

---
+++ UsersManagement/applications/users/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+# Gestión de usuarios
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+# Managers
+#from .managers import UserManager
+
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    """ Gestión de usuarios """
+
+    GENDER_CHOICES=(
+        ('M','MASCULINO'),
+        ('F', 'FEMENINO'),
+        ('O', 'OTRO'),
+    )
+
+    username = models.CharField(max_length=10,unique=True)
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=30,blank=True)
+    last_name = models.CharField(max_length=30,blank=True)
+    gender = models.CharField(max_length=1,choices=GENDER_CHOICES,blank=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', ]
+
+    #Instanciamos nuestro manager
+    #objects=UserManager()
+
+
+    def __str__(self):
+        return self.username
