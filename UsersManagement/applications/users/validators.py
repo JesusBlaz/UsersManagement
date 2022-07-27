103ef144469ce91bbe5a3b43b8e766ea117bcd4c
Jesus Garrido Marquez <jesus.garrido2015@gmail.com>
2022-07-27 04:07:40 -0500

    Utilizamos expresión regular para validar el curp.

--- UsersManagement/applications/users/validators.py
+++ UsersManagement/applications/users/validators.py
@@ -12,9 +12,12 @@ from django.core.exceptions import ValidationError
 def curp_validation(value):
     """ Función que valida un curp """
 
-    curp_validada = CURP(value)
-    if not curp_validada:
-        raise CURPValueError
+    curp_validada = "^[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$"
+
+    compilar = re.compile(curp_validada)
+    comparar = compilar.match(value)
+    if comparar is None:
+        raise ValidationError('CURP incorrecto')
 
 
 # RFC
