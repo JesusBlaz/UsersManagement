aa6ea8673929b7ea2295c65a79ca8271ac172621
Jesus Garrido Marquez <jesus.garrido2015@gmail.com>
2022-07-27 02:56:43 -0500

    Agregamos una nueva validación para el código postal.

--- UsersManagement/applications/users/validators.py
+++ UsersManagement/applications/users/validators.py
@@ -18,14 +18,25 @@ def curp_validation(value):
 
 
 # RFC
-rfc_pattern_pf = "^(([A-ZÑ&]{4})([0-9]{2})([0][13578]|[1][02])(([0][1-9]|[12][\\d])|[3][01])([A-Z0-9]{3}))|(([A-ZÑ&]{4})([0-9]{2})([0][13456789]|[1][012])(([0][1-9]|[12][\\d])|[3][0])([A-Z0-9]{3}))|(([A-ZÑ&]{4})([02468][048]|[13579][26])[0][2]([0][1-9]|[12][\\d])([A-Z0-9]{3}))|(([A-ZÑ&]{4})([0-9]{2})[0][2]([0][1-9]|[1][0-9]|[2][0-8])([A-Z0-9]{3}))$"
-
 def rfc_validation(value):
     """ Función que valida el RFC (Expresión regular) """
+
+    rfc_pattern_pf = "^(([A-ZÑ&]{4})([0-9]{2})([0][13578]|[1][02])(([0][1-9]|[12][\\d])|[3][01])([A-Z0-9]{3}))|(([A-ZÑ&]{4})([0-9]{2})([0][13456789]|[1][012])(([0][1-9]|[12][\\d])|[3][0])([A-Z0-9]{3}))|(([A-ZÑ&]{4})([02468][048]|[13579][26])[0][2]([0][1-9]|[12][\\d])([A-Z0-9]{3}))|(([A-ZÑ&]{4})([0-9]{2})[0][2]([0][1-9]|[1][0-9]|[2][0-8])([A-Z0-9]{3}))$"
 
     compilar = re.compile(rfc_pattern_pf)
     comparar = compilar.match(value)
     if comparar is None:
         raise ValidationError('RFC incorrecto')
 
+
+# CP
+def cp_validation(value):
+    """ Función que valida el Código postal """
+
+    cp_regex = "[0-9]{5}"
+
+    compilar = re.compile(cp_regex)
+    comparar = compilar.match(value)
+    if comparar is None:
+        raise ValidationError('CP incorrecto')
 
