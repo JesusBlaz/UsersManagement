d22ce2200b360d3fc1bff0bac9c8c75939901353
Jesus Garrido Marquez <jesus.garrido2015@gmail.com>
2022-07-31 17:07:54 -0500

    Agregamos nuestro router de nuestro viewset para los usuarios.

---
+++ UsersManagement/applications/users/routers.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import DefaultRouter
+
+from . import viewsets
+
+
+
+router = DefaultRouter()
+
+router.register(r'users', viewsets.UserViewSet, basename="users")
+
+
+urlpatterns = router.urls
